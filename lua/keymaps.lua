3591eaa66587b67ac2851d6a51eb33358fb3b774
thehnm <thehnm@posteo.net>
2024-01-30 11:45:35 +0100

    feat: add keymaps for switching buffers

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -3,3 +3,7 @@ local ExecuteShellCommandInBuffer = require("usercmd").ExecuteShellCommandInBuff
 
 vim.api.nvim_create_user_command("RunShellCommand", ExecuteShellCommandInBuffer, {})
 mapkeyret("<leader>e", "RunShellCommand", "Run shell command")
+
+mapkeyret("<leader>sn", "bnext", "Switch to next buffer")
+mapkeyret("<leader>sp", "bprevious", "Switch to previous buffer")
+vim.api.nvim_set_keymap("t", "<C-e>", [[<C-\><C-n>]], { noremap = true })
