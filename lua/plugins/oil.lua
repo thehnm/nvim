d5fcd1647130eccaf3c9032b2163c84fe74bb1b9
thehnm <thehnm@posteo.net>
2024-04-29 22:25:31 +0200

    feat: show permissions, size and mtime in oil

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -5,7 +5,16 @@ return {
     lazy = false,
     opts = {},
     dependencies = { "nvim-tree/nvim-web-devicons" },
-    config = true,
+    config = function()
+        require("oil").setup({
+            columns = {
+                "icon",
+                "permissions",
+                "size",
+                "mtime",
+            },
+        })
+    end,
     keys = {
         mapkeyret("-", "Oil", "Open Oil File Explorer"),
     },
