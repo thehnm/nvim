843265223dbebc3d39e43a3545edae90fd147a5e
thehnm <thehnm@posteo.net>
2025-02-19 00:20:44 +0100

    feat: add no-neck-pain plugin

---
+++ lua/plugins/no-neck-pain.lua
@@ -0,0 +1,15 @@
+local mapkeyret = require("utils.mapkey").mapkeyret
+
+return {
+    "shortcuts/no-neck-pain.nvim",
+    version = "*",
+    lazy = false,
+    config = function()
+        require("no-neck-pain").setup({
+            width = 180,
+        })
+    end,
+    keys = {
+        mapkeyret("<leader>nnp", "NoNeckPain", "Toggles the No Neck Pain plugin"),
+    },
+}
