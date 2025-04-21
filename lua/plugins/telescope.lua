f90fa4f28e77b413c8e48adfae15073471c3b949
thehnm <thehnm@posteo.net>
2025-04-21 20:18:30 +0200

    feat: update telescope

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -31,7 +31,8 @@ end
 
 return {
     "nvim-telescope/telescope.nvim",
-    tag = "0.1.5",
+    tag = "0.1.8",
+    branch = "0.1.x",
     lazy = false,
     dependencies = { "nvim-lua/plenary.nvim" },
     config = config,
