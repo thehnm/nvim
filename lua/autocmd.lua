7c667d4aed677c3f64d9dab06dd14194111e32af
thehnm <thehnm@posteo.net>
2025-05-02 09:59:23 +0200

    feat!: manually format buffer

--- lua/autocmd.lua
+++ lua/autocmd.lua
@@ -6,17 +6,17 @@ vim.api.nvim_create_autocmd("TextYankPost", {
     desc = "Highlight yanked text",
 })
 
-local lsp_fmt_group = vim.api.nvim_create_augroup("LspFormattingGroup", {})
-vim.api.nvim_create_autocmd("BufWritePost", {
-    group = lsp_fmt_group,
-    callback = function()
-        local efm = vim.lsp.get_clients({ name = "efm" })
-        if vim.tbl_isempty(efm) then
-            return
-        end
-        vim.lsp.buf.format({ name = "efm", async = true })
-    end,
-})
+-- local lsp_fmt_group = vim.api.nvim_create_augroup("LspFormattingGroup", {})
+-- vim.api.nvim_create_autocmd("BufWritePost", {
+--     group = lsp_fmt_group,
+--     callback = function()
+--         local efm = vim.lsp.get_clients({ name = "efm" })
+--         if vim.tbl_isempty(efm) then
+--             return
+--         end
+--         vim.lsp.buf.format({ name = "efm", async = true })
+--     end,
+-- })
 
 vim.api.nvim_create_autocmd("DiagnosticChanged", {
     pattern = "*",
--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -1,4 +1,5 @@
 local mapkeyret = require("utils.mapkey").mapkeyret
+local mapkey = require("utils.mapkey").mapkey
 local ExecuteShellCommandInBuffer = require("usercmd").ExecuteShellCommandInBuffer
 
 vim.api.nvim_create_user_command("RunShellCommand", ExecuteShellCommandInBuffer, {})
@@ -7,3 +8,12 @@ mapkeyret("<leader>e", "RunShellCommand", "Run shell command")
 mapkeyret("<leader>sn", "bnext", "Switch to next buffer")
 mapkeyret("<leader>sp", "bprevious", "Switch to previous buffer")
 vim.api.nvim_set_keymap("t", "<C-e>", [[<C-\><C-n>]], { noremap = true })
+
+local formatBuffer = function()
+    local efm = vim.lsp.get_clients({ name = "efm" })
+    if vim.tbl_isempty(efm) then
+        return
+    end
+    vim.lsp.buf.format({ name = "efm", async = true })
+end
+mapkey("<leader>fw", formatBuffer, "Format current buffer")
